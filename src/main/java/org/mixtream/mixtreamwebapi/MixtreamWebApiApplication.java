eeb1b3e2b3aeb91d91511229329ee7af7f0922cb
Paul Tumwine <paulatumwine@gmail.com>
2021-03-17 11:39:14 -0500

    initial commit

---
+++ src/main/java/org/mixtream/mixtreamwebapi/MixtreamWebApiApplication.java
@@ -0,0 +1,13 @@
+package org.mixtream.mixtreamwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MixtreamWebApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MixtreamWebApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mixtream/mixtreamwebapi/MixtreamWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mixtream.mixtreamwebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MixtreamWebApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
